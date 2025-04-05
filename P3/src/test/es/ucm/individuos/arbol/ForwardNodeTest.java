471f0ec1781804a9667e55048e1845ef438f51ec
Artem <artemvar@ucm.es>
2025-04-05 23:20:20 +0200

    test forward node

--- P3/src/test/es/ucm/individuos/arbol/ForwardNodeTest.java
+++ P3/src/test/es/ucm/individuos/arbol/ForwardNodeTest.java
@@ -11,4 +11,12 @@ public class ForwardNodeTest extends AbstractNodeTest {
         this.checkCorrectPath(new ForwardNode(), new Coord(1, 1), DirectionEnum.WEST, new Coord(1, 0), DirectionEnum.WEST);
         this.checkCorrectPath(new ForwardNode(), new Coord(1, 1), DirectionEnum.SOUTH, new Coord(2, 1), DirectionEnum.SOUTH);
     }
+
+    @Test
+    public void test2() {
+        this.checkCorrectPath(new ForwardNode(), new Coord(0, 0), DirectionEnum.WEST, new Coord(0, 31), DirectionEnum.WEST);
+        this.checkCorrectPath(new ForwardNode(), new Coord(0, 0), DirectionEnum.NORTH, new Coord(31, 0), DirectionEnum.NORTH);
+        this.checkCorrectPath(new ForwardNode(), new Coord(31, 31), DirectionEnum.EAST, new Coord(31, 0), DirectionEnum.EAST);
+        this.checkCorrectPath(new ForwardNode(), new Coord(31, 31), DirectionEnum.SOUTH, new Coord(0, 31), DirectionEnum.SOUTH);
+    }
 }
